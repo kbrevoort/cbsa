c0a29dde9e0aca99ee08e36a4abcc7b06e24ad53
Kenneth Brevoort <kenneth.brevoort@cfpb.gov>
2017-07-25 11:28:45 -0400

    Removed @import tidycensus roxygen command as it was requiring tidycensus for the package to install.

--- R/importing_data.R
+++ R/importing_data.R
@@ -221,8 +221,10 @@ convert2numeric <- function(df) {
 #' This function uses the tidycensus package to import data on median family incomes
 #' at the Census tract level from either the 2000 Decennial Census or the appropriate
 #' 5-year American Community Survey.
-#' @import tidycensus
 import_census <- function() {
+  if (!('tidycensus' %in% installed.packages()))
+    stop('import_census function only works when tidycensus is installed.')
+
   acs_data <- lapply(c(2010L, 2015L), download_acs) %>%
     bind_rows()
 
